629f8e3b26f7c438c5117c9a36559676f8806ba5
Mark Feng <markselbyfcy@gmail.com>
2019-11-12 21:46:47 +0800

    Add shape
    1. Add `get_shape` with some constants about shape
    2. Add Square and S shape first

--- src/tetris.rs
+++ src/tetris.rs
@@ -18,6 +18,39 @@ pub enum Cell {
     Placed = 2,  // already dropped cell
 }
 
+type ShapeData = Vec<(i32, i32)>;
+
+struct Shape {
+    data: ShapeData,
+    height: usize,
+}
+
+fn build_shape(data: ShapeData, height: usize) -> Shape {
+    Shape {
+        data,
+        height,
+    }
+}
+
+pub enum ShapeType {
+    // consider the top-center point as the bottom-center of the shape
+    Square,
+    S,
+    Z,
+    T,
+    L,
+    Line,
+    MirroredL,
+}
+
+fn get_shape(shape_type: ShapeType) -> Shape {
+    match shape_type {
+        ShapeType::Square => build_shape(vec![(0, 0), (0, 1), (1, 0), (1, 1)], 2),
+        ShapeType::S => build_shape(vec![(0, 0), (0, 1), (1, -1), (1, 0)], 2),
+        _ => build_shape(vec![], 0),
+    }
+}
+
 pub struct Board {
     width: usize,
     height: usize,
@@ -41,10 +74,13 @@ impl Board {
         &self.cells[x][y]
     }
 
-    fn add_shape(&mut self) {
+    fn add_shape(&mut self, shape: Shape) {
         // add a shape into the board, which should appear in the middle of the top row
         let mut next = self.cells.clone();
-        next[0][self.width / 2] = Cell::Running;
+        for (delta_x, delta_y) in shape.data {
+            next[(0 + delta_x) as usize][((self.width as i32 - 1) / 2 + delta_y) as usize]
+                = Cell::Running;
+        }
         self.cells = next;
     }
 
@@ -81,10 +117,24 @@ mod tests {
     }
 
     #[test]
-    fn test_add_shape() {
-        let mut board = Board::new(7, 10).unwrap();
-        board.add_shape();
+    fn test_add_shape_square() {
+        let mut board = Board::new(8, 10).unwrap();
+        board.add_shape(get_shape(ShapeType::Square));
+        assert_eq!(*board.get_cell(0, 3), Cell::Running);
+        assert_eq!(*board.get_cell(0, 4), Cell::Running);
+        assert_eq!(*board.get_cell(1, 3), Cell::Running);
+        assert_eq!(*board.get_cell(1, 4), Cell::Running);
+    }
+
+    #[test]
+    fn test_add_shape_s() {
+        let mut board = Board::new(8, 10).unwrap();
+        board.add_shape(get_shape(ShapeType::S));
         println!("{}", board);
-        assert_eq!(*board.get_cell(0, board.width / 2), Cell::Running);
+        assert_eq!(*board.get_cell(0, 3), Cell::Running);
+        assert_eq!(*board.get_cell(0, 4), Cell::Running);
+        assert_eq!(*board.get_cell(1, 2), Cell::Running);
+        assert_eq!(*board.get_cell(1, 3), Cell::Running);
+        assert_eq!(*board.get_cell(1, 4), Cell::Empty);
     }
 }
