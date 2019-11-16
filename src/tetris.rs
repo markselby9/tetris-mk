39dd36f337a09d6bce642093a8f78b8614b71191
Mark's alicloud instance <markselbyfcy@gmail.com>
2019-11-16 17:02:32 +0800

    Add drop function inside tick

--- src/tetris.rs
+++ src/tetris.rs
@@ -74,6 +74,10 @@ impl Board {
         &self.cells[x][y]
     }
 
+    fn set_cell(&mut self, x: usize, y: usize, val: Cell) {
+        self.cells[x][y] = val;
+    }
+
     fn add_shape(&mut self, shape: Shape) {
         // add a shape into the board, which should appear in the middle of the top row
         let mut next = self.cells.clone();
@@ -83,9 +87,45 @@ impl Board {
         }
         self.cells = next;
     }
+    
+    fn drop(&mut self) -> bool {
+        let mut next = self.cells.clone();
+        for i in (0..self.height).rev() {
+            for j in 0..self.width {
+                if next[i][j] == Cell::Running {
+                    // try to drop this cell
+                    if i == self.height - 1 || next[i+1][j] != Cell::Empty {
+                        //TODO: cannot drop, turn all the shape into Placed
+                        return false;
+                    }
+                    next[i+1][j] = Cell::Running;
+                    next[i][j] = Cell::Empty;
+                }
+            }
+        }
+        self.cells = next;
+        return true;
+    }
 
-    fn tick() {
+    fn move_shape(&mut self) {
+    }
+
+    fn is_ith_column_empty(&self, i:usize) -> bool {
+        self.cells[i].iter().all(|&x| x == Cell::Empty)
+    }
+
+    fn tick(&mut self) {
         //  what happens in the next frame
+        let try_drop = self.drop();
+        if !try_drop {
+            //todo: check whether game is already over
+            if (!self.is_ith_column_empty(0)) {
+                println!("GG!!");
+            }
+            else {
+                self.add_shape(get_shape(ShapeType::Square));
+            }
+        }
     }
 }
 
@@ -93,7 +133,11 @@ impl fmt::Display for Board {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         for line in self.cells.as_slice() {
             for &pos in line.as_slice() {
-                let symbol = if pos == Cell::Placed || pos == Cell::Running { "|x|" } else { "| |" };
+                let symbol = match pos {
+                    Cell::Placed => "|x|",
+                    Cell::Running => "|*|",
+                    _ => "| |",
+                };
                 write!(f, "{}", symbol);
             }
             write!(f, "\n");
@@ -130,11 +174,58 @@ mod tests {
     fn test_add_shape_s() {
         let mut board = Board::new(8, 10).unwrap();
         board.add_shape(get_shape(ShapeType::S));
-        println!("{}", board);
         assert_eq!(*board.get_cell(0, 3), Cell::Running);
         assert_eq!(*board.get_cell(0, 4), Cell::Running);
         assert_eq!(*board.get_cell(1, 2), Cell::Running);
         assert_eq!(*board.get_cell(1, 3), Cell::Running);
         assert_eq!(*board.get_cell(1, 4), Cell::Empty);
+    }
+
+    #[test]
+    fn test_drop() {
+        let mut board = Board::new(8, 10).unwrap();
+        board.add_shape(get_shape(ShapeType::S));
+        board.drop();
+        board.drop();
+        assert_eq!(*board.get_cell(1, 4), Cell::Empty);
+        assert_eq!(*board.get_cell(2, 3), Cell::Running);
+        assert_eq!(*board.get_cell(2, 4), Cell::Running);
+        assert_eq!(*board.get_cell(3, 2), Cell::Running);
+        assert_eq!(*board.get_cell(3, 3), Cell::Running);
+        assert_eq!(*board.get_cell(3, 4), Cell::Empty);
+    }
+
+    #[test]
+    fn test_cannot_drop() {
+        let mut board = Board::new(8, 10).unwrap();
+        for i in 0..8 {
+            board.set_cell(3, i, Cell::Placed);
+        }
+        board.add_shape(get_shape(ShapeType::Square));
+        assert_eq!(board.drop(), true);
+        assert_eq!(board.drop(), false); // cannot drop here
+    }
+
+    #[test]
+    fn test_tick() {
+        let mut board = Board::new(8, 10).unwrap();
+        board.add_shape(get_shape(ShapeType::Square));
+
+        println!("{}", board);
+        board.tick();
+        board.tick();
+        board.tick();
+        board.tick();
+        board.tick();
+        println!("{}", board);
+        board.tick();
+        board.tick();
+        board.tick();
+        board.tick();
+        println!("{}", board);
+        board.tick();
+        board.tick();
+        board.tick();
+        println!("{}", board);
     }
 }
